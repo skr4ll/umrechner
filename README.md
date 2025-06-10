f33f58aefb8e15764be44ebb8adf58794ebcefec
graef <simonjn45@gmail.com>
2025-06-10 14:18:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Aufgabe 2 -- Calculator
+
+Erstelle eine Android-App, die eine benutzungsfreundliche Oberfläche hat und eine bestimmte Umrechnung durchführt.
+
+- Flächen in Fußballfelder: Die App nimmt eine Flächengröße (z. B. in Quadratmetern) und zeigt an, wie viele Fußballfelder das sind.
+- Alter in Minuten: Die App fragt das Geburtsdatum ab, berechnet das aktuelle Alter und wie vielen Minuten das entspricht.
+- Geld in Zeit zur Veranschaulichung von großen Beträgen: Die App fragt nach einem Geldbetrag und gibt Tage bzw. Jahre aus.
+  Hintergrund: Um sich den Unterschied zwischen einer Million und einer Milliarde besser vorzustellen zu können, kann das Beispiel mit Sekunden helfen: 11 Tage haben 1 Million Sekunden. Eine Milliarde Sekunden hingegen sind 31,7 Jahre!
+
+## Vorbereitung
+
+Erstelle ein neues Android-Projekt in Android Studio und wähle Kotlin als Programmiersprache.
+Erstelle ein einfaches User Interface (UI) mit einem Eingabefeld und einem Button.
+
+## Funktionale Anforderungen
+
+1. Benutzende geben einen Wert ein (z. B. Geburtsdatum, Fläche oder Geldbetrag).
+2. App führt eine Umrechnung durch (z. B. Alter in Minuten, Fläche in Fußballfelder, Geld in Zeit).
+3. Das Ergebnis wird als lesbarer Text ausgegeben.
+
+## Beispiel:
+
+- Eingabe: "25 Jahre" → Ausgabe: "Das entspricht ca. 13.140.000 Minuten."
+- Eingabe: "5000 m²" → Ausgabe: "Das entspricht ca. 0,7 Fußballfeldern."
+- Eingabe: "1.000.000" → Ausgabe: "Das entspricht ca. 11 Tagen."
+
+
+## Extra-Herausforderungen (optional)
+
+Wenn du die Grundlagen gemeistert hast, kannst du noch folgende Verbesserungen einbauen:
+
+Dropdown-Menü für verschiedene Umrechnungen (der Nutzende können zwischen verschiedenen Optionen wählen).
+Fehlermeldungen anzeigen, wenn jemand keine Zahl eingibt.
+Design verbessern mit Farben, Icons oder Animationen.
---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.calculator.ui.theme.CalculatorTheme
+import kotlin.math.floor
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Create the Calculator.
+                    Calculator(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+fun calcPitch(area: Float): String {
+    var calculatedArea = area / 7_140
+    return "Das entspricht ca. $calculatedArea Fußballfeldern."
+}
+
+fun calcAge(years: Int): String {
+    // convert to double so it is easier to not convert it 3 times in a row
+    var y = years.toDouble()
+    var leapYears = floor(y / 4).toInt()
+    var leapHundredYears = floor(y / 100).toInt()
+    var leapFourHundredYears = floor(y / 400).toInt()
+    // first days times years
+    // then add all the leap years minus then 100 leap years plus the 400 leap years
+    var days: Int = (years * 365) + (leapYears - leapHundredYears + leapFourHundredYears)
+    var minutes: Int = days * 1440
+    return "Das entspricht ca. $minutes Minuten."
+}
+
+fun calcMoney(cents: Int): String {
+    var days = floor(cents.toDouble() / 86400)
+    return "Das entspricht ca. ${days.toInt()} Tagen."
+}
+
+fun getInteger(str: String): Int {
+    return (str.filter { it.isDigit() }).toInt()
+}
+
+@Composable
+fun Calculator(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+    var resolution by remember { mutableStateOf("") }
+    var padding = Modifier.padding(horizontal = 5.dp)
+
+    Box(modifier = modifier) {
+        Column(modifier = padding) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Amount") },
+                modifier = padding
+            )
+            Button(onClick = {
+                resolution = calcPitch(getInteger(text).toFloat())
+            }) { Text("Fläche in Fußballfelder") }
+            Button(onClick = {
+                resolution = calcAge(getInteger(text))
+            }) { Text("Alter in Minuten") }
+            Button(onClick = {
+                resolution = calcMoney(getInteger(text))
+            }) { Text("Geld in Zeit") }
+            OutlinedTextField(
+                value = resolution,
+                readOnly = true,
+                onValueChange = { resolution = it },
+                label = { Text("Ergebniss") })
+        }
+    }
+}
